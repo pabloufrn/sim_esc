35d9e04f58456e382dd68fdfc4ec9ceb07a9a091
Pablo Emanuell Lopes Targino <pabloemanuell@A101-09L.imd.local>
2019-11-13 17:56:51 -0300

    fifo2 começando

--- main.py
+++ main.py
@@ -4,15 +4,47 @@ import numpy as np
 
 
 def simular(alg, strref, qtdfrm, qtdpag):
+	if(qtdfrm >= qtdpag): 
+		print("Erro: Nenhuma página será substituida se qtdfrm >= qtdpag.")
+		exit(0)
 	if(alg == "fifo"):
 		return simular_fifo(strref, qtdfrm, qtdpag)
 	elif(alg == "fifo2"):
-		pass
+		return simular_fifo2(strref, qtdfrm, qtdpag)
 	elif(alg == "lru"):
 		pass
 	else:
 		print("Erro: forneca um algoritmo válido.")
 		return None
+
+def simular_fifo2(strref, qtdfrm, qtdpag):
+	refereced = qtdfrm * [False]
+	fila_frm = deque(qtdfrm * [''])
+	fila_prc = deque((qtdpag-qtdfrm) * ['']) # caso qtdfrm > qtdpag não vai ter substituição de páginas
+	tabela_prc = [list(fila_prc + fila_frm)]
+	page_fault = ['']
+	for page in strref:
+		if(page not in fila_frm):
+			
+			removed_index = 0
+			for i, p in enumerate(fila_frm):
+				if(refereced[i]):
+					refereced[i] = False
+				else:
+					refereced[i] = False
+					removed_index = i
+					break
+			try:
+				fila_prc.remove(page)
+			except:
+				fila_prc.pop()
+			fila_prc.appendleft('')
+			fila_frm[removed_index] = page
+			page_fault.append('P')
+		else:
+			page_fault.append('')
+		tabela_prc.append(list(fila_frm + fila_prc))
+	return tabela_prc, page_fault
 
 def simular_fifo(strref, qtdfrm, qtdpag):
 	fila_frm = deque(qtdfrm * [''])
@@ -50,8 +82,5 @@ if ( __name__ == "__main__"):
 	qtdpag = max(strref) - min(strref) + 1
 	qtdfrm = int(input("digite a quantidade de frames: "))
 	alg = input("digite a politíca desejada: (fifo, lru ou fifo2): ")
-	
 	tabela, page_fault = simular(alg, strref, qtdfrm, qtdpag)
 	salvar_em_csv("saida.csv", tabela, qtdfrm, page_fault, strref)
-
-
