8d0aecb5560a252b6817b6d5d8d698fbe8d8a0ef
Pablo E. L. Targino <pabloemanuell2017@gmail.com>
2019-11-24 23:26:57 -0300

    calcular string de distancia

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def simular(alg, strref, qtdfrm, qtdpag):
 	if(alg == "fifo"):
 		return simular_fifo(strref, qtdfrm, qtdpag)
 	elif(alg == "fifo2"):
-		pass
+		return simular_fifo2(strref, qtdfrm, qtdpag)
 	elif(alg == "lru"):
 		return simular_lru(strref, qtdfrm, qtdpag)
 	else:
@@ -31,9 +31,51 @@ def simular_fifo(strref, qtdfrm, qtdpag):
 		tabela_prc.append(list(fila_prc))
 	return tabela_prc, page_fault
 
+def simular_fifo2(strref, qtdfrm, qtdpag):
+	refereced = qtdfrm * [False]
+	fila_frm = deque(qtdfrm * [''])
+	fila_prc = deque((qtdpag-qtdfrm) * ['']) # caso qtdfrm > qtdpag não vai ter substituição de páginas
+	tabela_prc = [list(fila_prc + fila_frm)]
+	page_fault = ['']
+	for page in strref:
+		print("\t".join((str(p) for p in fila_frm)))
+		print("\t".join((str(r) for r in refereced)))
+		if(page not in fila_frm):
+			if(fila_frm[-1] == ''):
+				fila_frm.appendleft(page)
+				fila_frm.pop()
+				page_fault.append('P')
+				refereced[1] = refereced[0]
+				refereced[0] = False
+			else:
+				removed_index = -1
+				for i in range(len(fila_frm)-1, -1, -1):
+					if(refereced[i]):
+						refereced[i] = False
+					else:
+						refereced[i] = False
+						removed_index = i
+						break
+				try:
+					fila_prc.remove(page)
+				except ValueError:
+					fila_prc.pop()
+				fila_prc.appendleft(fila_frm[removed_index])
+				fila_frm.remove(fila_frm[removed_index])
+				fila_frm.appendleft(page)
+				page_fault.append('P')
+				for i in range(removed_index, 0, -1):
+					refereced[removed_index] = refereced[removed_index-1]
+				refereced[0] = False
+		else:
+			page_fault.append('')
+			refereced[fila_frm.index(page)] = True
+		tabela_prc.append(list(fila_frm + fila_prc))
+	return tabela_prc, page_fault
+
 def simular_lru(strref, qtdfrm, qtdpag):
 	fila_frm = deque(qtdfrm * [''])
-	fila_prc = deque(qtdpag * [''])
+	fila_prc = deque((qtdpag if qtdpag > qtdfrm else qtdfrm) * [''])
 	tabela_prc = [list(fila_prc)]
 	page_fault = ['']
 	for page in strref:
@@ -68,7 +110,7 @@ def calcular_string_distancia(tabela, strref):
 def calcular_media(list_dist):
 	return sum(list_dist)/len(list_dist)
 
-def salvar_em_csv(nome_arquivo, tabela, qtdframes, page_fault, strref):
+def salvar_em_csv(nome_arquivo, tabela, qtdframes, page_fault, strref, string_dist):
 	tabela = np.array(tabela).T
 	with open(nome_arquivo, "w+") as f:
 		writer = csv.writer(f)
@@ -80,6 +122,7 @@ def salvar_em_csv(nome_arquivo, tabela, qtdframes, page_fault, strref):
 		for line in tabela[qtdframes:]:
 			writer.writerow(line)
 		writer.writerow(page_fault)
+		writer.writerow([''] + string_dist)
 
 if ( __name__ == "__main__"):
 	strstrref = input("digite a string de referência: \n")
@@ -96,6 +139,6 @@ if ( __name__ == "__main__"):
 	print(string_dist)
 	print("\nMédia: ")
 	print(media)
-	salvar_em_csv("saida.csv", tabela, qtdfrm, page_fault, strref)
+	salvar_em_csv("saida.csv", tabela, qtdfrm, page_fault, strref, string_dist)
 
 
